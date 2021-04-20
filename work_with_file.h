c5cee9a32d85e28898bc4c99adbc09076c3d4a13
Sviatoslav <slavik2003>
2021-04-20 14:06:13 +0300

    wor_with_file

--- work_with_file.h
+++ work_with_file.h
@@ -1,9 +1,16 @@
 #pragma once
 #include<iostream>
 #include<fstream>
+#include<cmath>
 
 using namespace std;
 
-int file_size (fstream &file);
+int file_size (string);
 
-char* get_bytes (string name);
+void read_uncoded (char *, string, int);
+
+void Write_coded (int *, int, int, string);
+
+void Read_coded (int *, string, int);
+
+void write_decoded (char *, string, int);
